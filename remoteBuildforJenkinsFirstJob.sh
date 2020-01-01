c28f6023f86ab49415aa8e012e1d59a194ed007f
praneeth <praneeth@ubuntu.com>
2019-12-31 21:48:17 -0800

    Created a shell script that runs jenkins job remotely

---
+++ remoteBuildforJenkinsFirstJob.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+jobName="$1"
+tokenName="$2"
+
+#firefox http://localhost:8080/job/myFirstJob/build?token=$tokenName
+echo $jobName
+echo $tokenName
+
+
+if [ "$1" == "-usage" ] 
+then
+	echo "./remoteBuildforJenkinsFirstJob <jobName> <passphrase>"
+	echo "<jobName> -----> job  name in Jenkins"
+	echo "<passphrase> -----> token name for the job remote build"
+	exit
+elif [ "$1" == "-help" ] 
+then
+	echo "enter ./remoteBuildforJenkinsFirstJob -usage for usage"
+	exit
+elif [ $# != 2 ] 
+then
+	echo "Error: See usage"
+	./remoteBuildforJenkinsFirstJob.sh -usage 
+    exit
+else
+	firefox http://localhost:8080/job/$jobName/build?token=$tokenName
+fi
+
